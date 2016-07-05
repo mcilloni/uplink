21bffc6c4f3ff0d3edb423c5f903b3a0e7a5cc2a
Marco Cilloni <marco.cilloni@yahoo.com>
2016-07-05 19:18:16 +0200

    Working on notifications

---
+++ uplink/dispatcher.go
@@ -0,0 +1,111 @@
+/*
+ *  uplink, a simple daemon to implement a simple chat protocol
+ *  Copyright (C) Marco Cilloni <marco.cilloni@yahoo.com> 2016
+ *
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *  Exhibit B is not attached; this software is compatible with the
+ *  licenses expressed under Section 1.12 of the MPL v2.
+ *
+ */
+
+package uplink
+
+import pd "github.com/mcilloni/uplink/protodef"
+
+type sink struct {
+	UID  int64
+	Sink chan<- *pd.Notification
+}
+
+type msg struct {
+	UID          int64
+	Notification *pd.Notification
+}
+
+type dispatcher struct {
+	bins    map[int64][]chan<- *pd.Notification
+	started bool
+
+	addSinkChan    chan *sink
+	notifyChan     chan *msg
+	removeSinkChan chan *sink
+}
+
+func startDispatcher() *dispatcher {
+	d := &dispatcher{
+		bins:           make(map[int64][]chan<- *pd.Notification),
+		addSinkChan:    make(chan *sink, 100),
+		notifyChan:     make(chan *msg, 1000),
+		removeSinkChan: make(chan *sink, 100),
+	}
+
+	go d.start()
+
+	return d
+}
+
+func (d *dispatcher) addSink(uid int64, sink chan<- *pd.Notification) {
+	bin, ok := d.bins[uid]
+	if !ok {
+		bin = []chan<- *pd.Notification{}
+	}
+
+	d.bins[uid] = append(bin, sink)
+}
+
+func (d *dispatcher) notify(uid int64, notif *pd.Notification) {
+	if bin, ok := d.bins[uid]; ok {
+		for _, sink := range bin {
+			go func(sink chan<- *pd.Notification) {
+				sink <- notif
+			}(sink)
+		}
+	}
+}
+
+func (d *dispatcher) removeSink(uid int64, toRemove chan<- *pd.Notification) {
+	if bin, ok := d.bins[uid]; ok {
+		for i, sink := range bin {
+			if sink == toRemove {
+				d.bins[uid] = append(bin[:i], bin[i+1:]...)
+
+				return
+			}
+		}
+	}
+}
+
+func (d *dispatcher) start() {
+	if d.started {
+		panic("dispatcher already started")
+	}
+
+	d.started = true
+
+	for {
+		select {
+		case sink := <-d.addSinkChan:
+			d.addSink(sink.UID, sink.Sink)
+
+		case msg := <-d.notifyChan:
+			d.notify(msg.UID, msg.Notification)
+
+		case sink := <-d.removeSinkChan:
+			d.RemoveSink(sink.UID, sink.Sink)
+		}
+	}
+}
+
+func (d *dispatcher) AddSink(uid int64, newSink chan<- *pd.Notification) {
+	d.addSinkChan <- &sink{uid, newSink}
+}
+
+func (d *dispatcher) Notify(uid int64, notification *pd.Notification) {
+	d.notifyChan <- &msg{uid, notification}
+}
+
+func (d *dispatcher) RemoveSink(uid int64, oldSink chan<- *pd.Notification) {
+	d.removeSinkChan <- &sink{uid, oldSink}
+}
